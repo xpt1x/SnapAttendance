129dc43aa4b69ab31e67c37b73ba52aea0361979
Jatin <jatinsarda007@gmail.com>
2020-07-14 12:13:53 +0530

    chnages

--- frontend/src/components/SingIn.js
+++ frontend/src/components/SingIn.js
@@ -7,7 +7,6 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import DashBoard from './DashBoard';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
