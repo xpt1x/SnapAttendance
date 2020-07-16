b25529516f199ab7c6c9b3de3091f107121768e4
Swarnim Doegar <swarnimdoegar@gmail.com>
2020-07-16 16:39:38 +0530

    pushing application.py

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_from_directory
 from flask_cors import CORS
 from uims_api import SessionUIMS
 
@@ -7,10 +7,15 @@ app = Flask(__name__, template_folder='frontend/build',
             static_folder='frontend/build/static')
 CORS(app)
 
+print(app.root_path)
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+@app.route('/<path:filename>')
+def base_static(filename):
+    return send_from_directory(app.root_path + '/frontend/build/', filename)
 
 @app.route('/api', methods=['POST'])
 def get_data():
