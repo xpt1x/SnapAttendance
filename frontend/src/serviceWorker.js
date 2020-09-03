2a130e730c2c9aacf8bc2adea356fe019864662a
Swarnim Doegar <swarnimdoegar@gmail.com>
2020-09-03 19:28:20 +0530

    made the app pwa again with changed version

--- frontend/src/serviceWorker.js
+++ frontend/src/serviceWorker.js
@@ -34,7 +34,7 @@ export function register(config) {
       return;
     }
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js?version=1`;
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js?version=2`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
