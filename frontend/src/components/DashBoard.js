8ab4d62824dcffd78e79217fcaf20c18f6ad1bc2
Jatin <jatinsarda007@gmail.com>
2020-09-08 12:16:25 +0530

    test mob

--- frontend/src/components/DashBoard.js
+++ frontend/src/components/DashBoard.js
@@ -84,7 +84,7 @@ function CircularProgressWithLabel(props) {
                 justifyContent="center"
             >
                 <Typography variant="h6" component="div" color="textPrimary">
-                    {props.lectures !== '0' ? props.value : <Typography color='textSecondary'> NA </Typography>}
+                    {props.lectures !== '0' ? props.value : <Typography color='textSecondary'> Not </Typography>}
                 </Typography>
             </Box>
         </Box>
