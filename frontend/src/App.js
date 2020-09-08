527c008329d3be9770ba0168e6908bec2a7aafeb
Jatin <jatinsarda007@gmail.com>
2020-09-08 12:07:16 +0530

    updated sw

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -55,14 +55,12 @@ class App extends Component {
 
   render() {
     return(
-      (this.globalTheme)?(
         <MuiThemeProvider theme={this.globalTheme}>
           <CssBaseline />
           {
             (localStorage.getItem('uid')) ? <DashBoard changeTheme={this.changeTheme} /> : <SignIn />
           }
         </MuiThemeProvider>
-      ):((localStorage.getItem('uid')) ? <DashBoard changeTheme={this.changeTheme} /> : <SignIn />)
     )
   }
 }
